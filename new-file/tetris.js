3dae57beffbb653ab36117676438144f4bee7bba
Paulina <p.kijowska@gmail.com>
2022-01-02 13:42:18 +1300

    move the block down

--- new-file/tetris.js
+++ new-file/tetris.js
@@ -5,9 +5,11 @@ const findBody = document.getElementsByTagName('body')[0]
 findBody.appendChild(para);
 console.log(para.getBoundingClientRect().y)
 //move the block
+let pos = 10;
 document.addEventListener("keydown", function(e) {
   if (e.key === 'ArrowDown') {
-    para.getBoundingClientRect().y +1;
-    console.log(  para.getBoundingClientRect().y)
+    // para.getBoundingClientRect().y +1;
+    para.style.top = pos + 10 + 'px';
+    pos = pos + 10;
   }
 });
