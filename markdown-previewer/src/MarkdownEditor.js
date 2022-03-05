8ced490c9ca7287952c7c5bcc8efc7f142bf1864
Paulina <p.kijowska@gmail.com>
2022-03-05 13:54:29 -0600

    adding

--- markdown-previewer/src/MarkdownEditor.js
+++ markdown-previewer/src/MarkdownEditor.js
@@ -6,12 +6,17 @@ class MarkdownEditor extends React.Component {
   constructor() {
     super()
     this.state = {
-      text: ['# Welcome to my React Markdown Previewer!', '\n## This is a subheading','\n### This is another subheading', '\nHeres some code, `<div> Hello</div>`, between 2 backticks.', '- And of course there are lists.  - Some are bulleted.  - With different indentation levels. - That look like this.', 'You can also make text **bold**... whoa!Or _italic_. Or... wait for it... **_both!_** And feel free to go crazy ~~crossing stuff out~~.']
+      text: `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.`
     }
-  }
 
-  componentDidMount() {
-
+    const options: MarkedOptions = {
+      gfm: true
+    }
   }
 
   handleOnChange = (e) => {
@@ -19,21 +24,24 @@ class MarkdownEditor extends React.Component {
     this.setState({
             text: splitText
           },() => {
-            console.log(this.state.text, 'checlforcode')
+            console.log(this.state.text, 'check for state')
     })
-
   }
 
   render() {
-    const final = this.state.text.join(',');
+    const finalText = this.state.text;
     return (
       <div className="form">
         <div className="form-group shadow-textarea">
-          <label for="editor">Editor</label>
-          <textarea onChange={this.handleOnChange} className="form-control z-depth-1" id="editor" rows="2" placeholder="Write something here..." >{final}</textarea>
+          <div className="header-wrapper">
+              <label for="editor">
+                <h4>Editor</h4>
+              </label>
+            <textarea onChange={this.handleOnChange} className="form-control z-depth-1" id="editor" rows="2" placeholder="Write something here..." >{finalText}</textarea>
+          </div>
         </div>
         <hr/>
-        <MarkdownPreviewer text={this.state.text} />
+        <MarkdownPreviewer options={this.options} text={this.state.text} />
       </div>
     )
   }
--- markdown-previewer/src/MarkdownPreviewer.js
+++ markdown-previewer/src/MarkdownPreviewer.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Markdown from 'marked-react';
+import remarkGfm from 'remark-gfm';
 
 const MarkdownPreviewer = (props) => {
+  let [editorMode, changeMode] = useState('editor-small')
+  const detectArr = Array.isArray(props.text) ? props.text : props.text.split('\n');
+
+  const handleClick = () => {
+    return editorMode === 'editor-small' ? changeMode('editor-big') : changeMode('editor-small');
+  }
+
   return (
-    <div>
-    {props.text.map(text=>{
-      return (<Markdown>{text}</Markdown>)
-    })}
-    </div>
+      <div className={`header-wrapper preview-wrapper ${editorMode}`}>
+        <div className="preview-header">
+          <h4>Previewer</h4>
+          <div onClick={handleClick}>*</div>
+        </div>
+        <div className="preview-inner">
+          {detectArr.map(text=>{
+            return (<Markdown>{text}</Markdown>)
+          })}
+        </div>
+      </div>
   )
 }
 
