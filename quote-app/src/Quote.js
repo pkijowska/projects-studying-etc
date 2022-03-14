65a446f577920832f2630da8ba03ec73f111348f
Paulina <p.kijowska@gmail.com>
2022-03-13 21:16:12 -0600

    refactor

--- quote-app/src/Quote.js
+++ quote-app/src/Quote.js
@@ -4,15 +4,16 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Quote = () => {
-  const [quote, setText] = useState({text: 'Spread love everywhere you go.', author: 'Mother Teresa'})
-  const [color, Color] = useState('')
+  const [quote, setText] = useState({text: 'Spread love everywhere you go.', author: 'Mother Teresa', load: false})
 
   const changeColor = () => {
     const bodyElt = document.querySelector("body");
     const button = document.querySelector("button");
     const bgcolor = "hsla(" + ~~(360 * Math.random())+","+"70%,"+"80%,1)"
+    if (button) {
+      button.style.backgroundColor = bgcolor
+    }
     bodyElt.style.backgroundColor = bgcolor;
-    button.style.backgroundColor = bgcolor
   }
 
   useEffect(() => {
@@ -30,14 +31,15 @@ const Quote = () => {
       const final = jsn[randomQuote].text;
       const author = jsn[randomQuote].author;
       changeColor()
-      return setText({text: final, author: author})
+      return setText({text: final, author: author, load: true})
     } catch {
-      console.log('error')
+      setText({load: false})
     }
   }, [])
 
   return (
     <div className="quote-wrapper d-flex justify-content-center align-items-center">
+      {quote.load ?
       <Card gap={2} className="col-md-6 mx-auto">
         <Card.Header>Quote</Card.Header>
         <Card.Body>
@@ -53,7 +55,7 @@ const Quote = () => {
           </blockquote>
           <Button variant="light" id="btn new-quote" className="mt-2" onClick={sendRequest}>New quote</Button>
         </Card.Body>
-      </Card>
+      </Card> : ''}
     </div>
   )
 }
