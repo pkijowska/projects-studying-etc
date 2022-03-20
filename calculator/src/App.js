e6708d9ed29f0b9e4374acac09a580bf1fc9163f
Paulina <p.kijowska@gmail.com>
2022-03-19 20:53:57 -0600

    calculator scheleton done

---
+++ calculator/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="calculator-wrapper">
+      <div className="calculator-wrapper__main">
+        <div className="calculcator-panel"> </div>
+        <div className="calculator-block"> AC </div>
+        <div className="calculator-block"> +/- </div>
+        <div className="calculator-block"> % </div>
+        <div className="calculator-block"> / </div>
+        <div className="calculator-block"> 7 </div>
+        <div className="calculator-block"> 8 </div>
+        <div className="calculator-block"> 9 </div>
+        <div className="calculator-block"> x </div>
+        <div className="calculator-block"> 4 </div>
+        <div className="calculator-block"> 5 </div>
+        <div className="calculator-block"> 6 </div>
+        <div className="calculator-block"> - </div>
+        <div className="calculator-block"> 1 </div>
+        <div className="calculator-block"> 2 </div>
+        <div className="calculator-block"> 3 </div>
+        <div className="calculator-block"> + </div>
+        <div className="calculator-block"> 0 </div>
+        <div className="calculator-block"> , </div>
+        <div className="calculator-block"> = </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
