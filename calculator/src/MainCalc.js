7175571f96b28d2c287737272519f7db2d13171c
Paulina <p.kijowska@gmail.com>
2022-04-04 18:57:21 -0500

    x operation works

--- calculator/src/MainCalc.js
+++ calculator/src/MainCalc.js
@@ -3,12 +3,9 @@ import React, { useState, useEffect } from 'react';
 const MainCalc = () => {
   let [display, setDisplay] = useState([0]);
   let [total, changeTotal] = useState([0]);
-  // let [number, changeNumber] = useState([]);
   let [number, showCurrent] = useState(false);
   let [final, showFinal] = useState(false);
-  //as you type show type display, when adding add total,
-  // let [singleNum, addSingleNum] = useState(0)
-  //operational values
+
   const handleClick = (e) => {
     showCurrent(true)
     const { innerText } = e.target;
@@ -16,25 +13,16 @@ const MainCalc = () => {
       const newArr = [...display];
       const [,...rest] = newArr;
       setDisplay([rest, innerText].join(''));
-      // changeNumber(Number([rest, innerText].join('')))
-      // addSingleNum([rest, innerText].join(''))
       console.log(total, 'what is total?')
-      // changeTotal(Number([rest, innerText].join('')))
-
     } else {
       setDisplay([...display, innerText].join(''));
       console.log('wtf', [...display, innerText].join(''), total)
-      // changeTotal(Number(total) + Number([...display, innerText].join('')))
-      // changeTotal()
-      // addSingleNum(0)
     }
   }
 
   const resetClick = () => {
-    // numberOperations([])
     setDisplay([0])
     changeTotal([0])
-    // changeNumber([])
   }
 
   const handlePlus = () => {
@@ -42,11 +30,6 @@ const MainCalc = () => {
     changeTotal([Number(total) + Number(display)])
     console.log(total, Number(total) + Number(display), display, 'total')
     setDisplay([0])
-    // pushNumbers(prevState => [...prevState, numbers]);
-    // setDisplay([Number(total) + number])
-
-    // Number(total) - Number(singleNum)
-    // setDisplay([0])
    }
 
    // // do I need this?
@@ -55,13 +38,18 @@ const MainCalc = () => {
    //   console.log('changed')
    // }, [total])
 
-   // const handleMinus = () => {
-   //   // pushNumbers(prevState => [...prevState, numbers]);
-   //   changeTotal(Number(total) - number)
-   //   // Number(total) - Number(singleNum)
-   //   console.log(total, number, 'minus')
-   //   setDisplay([0])
-   //  }
+   const handleTimes = () => {
+     // pushNumbers(prevState => [...prevState, numbers]);
+     showCurrent(false)
+     console.log('display', total)
+     if (total[0] !== 0) {
+       changeTotal([Number(total) * Number(display)])
+     } else {
+       changeTotal([Number(display)])
+     }
+     console.log(total, display, Number(total) * Number(display), 'times')
+     setDisplay([0])
+    }
 
   const handleResult = () => {
     setDisplay(total)
@@ -88,11 +76,11 @@ const MainCalc = () => {
         <div onClick={handleClick} className="calculator-block"> 7 </div>
         <div onClick={handleClick} className="calculator-block"> 8 </div>
         <div onClick={handleClick} className="calculator-block"> 9 </div>
-        <div onClick={handleClick} className="calculator-block"> x </div>
+        <div onClick={handleTimes} className="calculator-block"> x </div>
         <div onClick={handleClick} className="calculator-block"> 4 </div>
         <div onClick={handleClick} className="calculator-block"> 5 </div>
         <div onClick={handleClick} className="calculator-block"> 6 </div>
-        <div  className="calculator-block"> - </div>
+        <div className="calculator-block"> - </div>
         <div onClick={handleClick} className="calculator-block"> 1 </div>
         <div onClick={handleClick} className="calculator-block"> 2 </div>
         <div onClick={handleClick} className="calculator-block"> 3 </div>
