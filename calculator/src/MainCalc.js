a087f30bcce185db25d63b2da69a43a768f32ed4
Paulina <p.kijowska@gmail.com>
2022-05-18 20:57:42 -0500

    calculator from start :(

--- calculator/src/MainCalc.js
+++ calculator/src/MainCalc.js
@@ -1,115 +1,124 @@
 import React, { useState, useEffect } from 'react';
 
 const MainCalc = () => {
-  let [start, changeInitialValue] = useState(true)
-  let [display, setDisplay] = useState([0]);
-  let [firstNumber, firstIsTaken] = useState(false);
-  let [total, changeTotal] = useState([0]);
-  let [number, showCurrent] = useState(false);
-  let [final, showFinal] = useState(false);
-  let [lastNumber, takeLastNumber] = useState([0]);
-  let [add, addNumber] = useState(false);
-  let [arr, changeArr] = useState([])
-
-  //display 1.. when typing add numbers [10, 100, 1005]
-
+  // let [start, changeInitialValue] = useState(true)
+  // let [display, setDisplay] = useState([0]);
+  // let [firstNumber, firstIsTaken] = useState(false);
+  // let [total, changeTotal] = useState([0]);
+  // let [number, showCurrent] = useState(false);
+  // let [final, showFinal] = useState(false);
+  // let [lastNumber, takeLastNumber] = useState([0]);
+  // let [add, addNumber] = useState(false);
+  // let [arr, changeArr] = useState([])
+  //
+  // //display 1.. when typing add numbers [10, 100, 1005]
+  //
   const handleClick = (e) => {
-    showCurrent(true)
-    const { innerText } = e.target;
-    changeInitialValue(false)
-    console.log([...display, innerText], 'inner')
-    setDisplay([...display, innerText].join(''))
-
-    if (!arr.length) {
-      changeArr([parseInt(display, 10)])
-      console.log('first', arr)
-    } else {
-      console.log(arr, 'array1')
-      let [first, ...mainArr] = arr
-      console.log(mainArr, 'mainArr')
-      changeArr([...arr, parseInt(display, 10)])
-    }
-    // arr = arr.concat([parseInt(display, 10)])
-    // console.log(arr, 'arr')
-    showCurrent(false)
-
-    if (add) {
-      console.log('add', total, parseInt(display, 10))
-    }
+    // showCurrent(true)
+    // const { innerText } = e.target;
+    // changeInitialValue(false)
+    // console.log([...display, innerText], 'inner')
+    // setDisplay([...display, innerText].join(''))
+    //
+    // if (!arr.length) {
+    //   changeArr([parseInt(display, 10)])
+    //   console.log('first', arr)
+    // } else {
+    //   console.log(arr, 'array1')
+    //   let [first, ...mainArr] = arr
+    //   console.log(mainArr, 'mainArr')
+    //   changeArr([...arr, parseInt(display, 10)])
+    // }
+    // // arr = arr.concat([parseInt(display, 10)])
+    // // console.log(arr, 'arr')
+    // showCurrent(false)
+    //
+    // if (add) {
+    //   console.log('add', total, parseInt(display, 10))
+    // }
   }
 
   const resetClick = () => {
-    setDisplay([0])
-    changeTotal([0])
-    changeInitialValue(true)
-    addNumber(false)
+    // setDisplay([0])
+    // changeTotal([0])
+    // changeInitialValue(true)
+    // addNumber(false)
   }
-
+  //
   const handlePlus = () => {
-    addNumber(true)
-
-    changeTotal(Number(total) + Number(display))
-
-    showCurrent(false)
-    showFinal(true)
-    firstIsTaken(true)
-    setDisplay([0])
+    // addNumber(true)
+    //
+    // changeTotal(Number(total) + Number(display))
+    //
+    // showCurrent(false)
+    // showFinal(true)
+    // firstIsTaken(true)
+    // setDisplay([0])
     //
     // // changeTotal([Number(total) + Number(display)])
     // console.log(total, 'total')
    }
-
-   // do I need this?
-   useEffect(() => {
-     // changeFinal(total)
-     // console.log(lastNumber, 'changed', total)
-     console.log(arr, 'rrr')
-
-   }, [arr])
-
+  //
+  //  // do I need this?
+  //  useEffect(() => {
+  //    // changeFinal(total)
+  //    // console.log(lastNumber, 'changed', total)
+  //    console.log(arr, 'rrr')
+  //
+  //  }, [arr])
+  //
    const handleTimes = () => {
-     addNumber(false)
-
-     // pushNumbers(prevState => [...prevState, numbers]);
-     showCurrent(false)
-     console.log(total, display, 'times')
-     // changeTotal([Number(display)])
-
-     setDisplay([0])
+     // addNumber(false)
+     //
+     // // pushNumbers(prevState => [...prevState, numbers]);
+     // showCurrent(false)
+     // console.log(total, display, 'times')
+     // // changeTotal([Number(display)])
+     //
+     // setDisplay([0])
     }
-
+  //
   const handleResult = () => {
-    setDisplay(total)
+    // setDisplay(total)
   }
-
-  // const showNumber = (num) => {
+  //
+  // // const showNumber = (num) => {
+  // //   if (start) {
+  // //     console.log('start')
+  // //     return 0
+  // //   } else if (number && (!firstNumber)) {
+  // //     console.log('second')
+  // //     return parseInt(display, 10)
+  // //   } else if (firstNumber) {
+  // //     return parseInt(sencondDisplay, 10)
+  // //   } else {
+  // //     console.log('fourth')
+  // //     return total
+  // //   }
+  // // }
+  //
+  // const showNumber = () => {
   //   if (start) {
-  //     console.log('start')
   //     return 0
-  //   } else if (number && (!firstNumber)) {
-  //     console.log('second')
-  //     return parseInt(display, 10)
-  //   } else if (firstNumber) {
-  //     return parseInt(sencondDisplay, 10)
   //   } else {
-  //     console.log('fourth')
-  //     return total
+  //     return parseInt(display, 10)
   //   }
   // }
 
-  const showNumber = () => {
-    if (start) {
-      return 0
-    } else {
-      return parseInt(display, 10)
-    }
-  }
+  // ***************************** \\
+  // Basically what needs to be done:
+  // 1. Check if the operation is first = if yes, then convert what is typed into total
+  // 2. if the operation is not first then check operator (if first operation check)
+  // 3. If the operator is clicked show total
+  // 4. What is being typed add to total.
+  // 5. Register the previous number and if there is more numbers added extract from total
+  // ***************************** \\
 
 
   return (
     <div className="calculator-wrapper">
       <div className="calculator-wrapper__main">
-        <div className="calculcator-panel">{showNumber()} </div>
+        <div className="calculcator-panel">Nothing yet </div>
         <div onClick={resetClick} className="calculator-block"> AC </div>
         <div onClick={handleClick} className="calculator-block"> +/- </div>
         <div onClick={handleClick} className="calculator-block"> % </div>
