d18da75e3bd694ace9eb0681f12443ef2e4374fd
Paulina <p.kijowska@gmail.com>
2022-06-15 15:46:54 +0200

    add almost working state

--- calendo/src/Card.js
+++ calendo/src/Card.js
@@ -2,7 +2,7 @@ import './Card.css';
 import React, { useState } from 'react';
 
 function Card(props) {
-  const [calendar, setCalendar] = useState({id: 15, open: 'hidden', calendarEvent: "Go to pilates classes"});
+  const [calendar, setCalendar] = useState([{id: 15, open: 'hidden', calendarEvent: "Go to pilates classes"}]);
 
   // const d = new Date(`${props.nameOfMonth} ${props.num+1}, ${props.year} 01:15:00`);
   // let day = d.getDay();
@@ -11,12 +11,76 @@ function Card(props) {
   // console.log(props.num === undefined ? props.prevMonthCount)
   const renderCurrentMonthCount = props.todayNr === props.num+1 ? <span className="card-today">{props.num+1}</span> : props.num+1;
   const checkForPreviousMonth = props.prevMonthCount ? props.prevMonthCount - props.num+1+props.iteration : renderCurrentMonthCount;
+
   const handleClick = (e) => {
-    console.log(props.num+1,'hey')
-    setCalendar({...calendar, open: 'open'})
+    // console.log(props.num+1,'hey')
+    // setCalendar([{open: 'open', id: props.num+1}, ...calendar])
   }
 
-  const todoSnippet = <div className={`todo ${calendar.open}`}> Todo: </div>
+  const handleTodo = (event) => {
+    var newData = [...calendar];
+    // newData[id].title = title;
+    // this.setState({newData});
+
+    newData.forEach((item, itemIndex) => {
+      // console.log(calendar, (props.num+1 === (item && item.id)), props.num+1, item && item.id)
+      console.log(item && item.id, props.num+1)
+      if (props.num+1 === (item && item.id)) {
+        newData[itemIndex].calendarEvent = event.target.value;
+        setCalendar(newData)
+        //do I need this?
+        // item.calendarEvent = event.target.value;
+        // let newState = {id: props.num+1, open: 'open', calendarEvent: event.target.value}
+        // return { ...newState }
+      } else {
+        let newState = {id: props.num+1, open: 'open', calendarEvent: event.target.value}
+        setCalendar([...calendar, newState])
+      }
+      console.log(calendar, 'cal')
+
+    })
+
+  }
+
+    // setCalendar((prevValue)=> {
+    //   return prevValue.map((item, itemIndex) => {
+    //   // console.log(calendar, (props.num+1 === (item && item.id)), props.num+1, item && item.id)
+    //   if (props.num+1 === (item && item.id)) {
+    //     console.log('hey1')
+    //     //do I need this?
+    //     // item.calendarEvent = event.target.value;
+    //     let newState = {id: props.num+1, open: 'open', calendarEvent: event.target.value}
+    //     return { ...newState }
+    //   } else {
+    //     let newState = {id: props.num+1, open: 'open', calendarEvent: event.target.value}
+    //
+    //     return [newState, {"a":1,"b":2,"c":3} ]
+    //   }
+
+      // } else {
+      //   console.log('hey2', calendar)
+      //   let newState = {id: props.num+1, open: 'open', calendarEvent: event.target.value}
+      //   return [...calendar,
+      //         {...newState }]
+      //
+        // }
+      // })
+//     }
+// )
+
+    // setCalendar([ {calendarEvent: event.target.value, id: props.num+1}, ...calendar ])
+    // const item = {calendarEvent: event.target.value, id: props.num+1, open: 'open'}
+    // const foundIndex = calendar.findIndex(x => x.id == item.id);
+    // items[foundIndex] = item;
+    // console.log(calendar, 'cal')
+  // }
+  // console.log(calendar.find((x)=>x===15))
+  const todoSnippet = <div className={`todo ${calendar.open}`}>  <textarea
+          value={calendar.calendarEvent}
+          onChange={handleTodo}
+          rows={5}
+          cols={5}
+        /> </div>
 
   return(
     <div onClick={handleClick} className="card">
