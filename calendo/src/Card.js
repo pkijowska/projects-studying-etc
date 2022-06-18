09ab7fdb505d287e950e529b53af9af8e03f3de3
Paulina <p.kijowska@gmail.com>
2022-06-18 14:07:03 +0200

    display when clickin on submit

--- calendo/src/Card.js
+++ calendo/src/Card.js
@@ -2,7 +2,7 @@ import './Card.css';
 import React, { useState } from 'react';
 
 function Card(props) {
-  const [calendar, setCalendar] = useState([{id: 15, open: 'hidden', calendarEvent: "Go to pilates classes"}]);
+  const [calendar, setCalendar] = useState([{id: 15, isSubmitted: true,open: 'hidden', calendarEvent: "Go to pilates classes"}]);
 
   const renderCurrentMonthCount = props.todayNr === props.num+1 ? <span className="card-today">{props.num+1}</span> : props.num+1;
   const checkForPreviousMonth = props.prevMonthCount ? props.prevMonthCount - props.num+1+props.iteration : renderCurrentMonthCount;
@@ -24,19 +24,35 @@ function Card(props) {
     //   return 'jkkk'
     // }
     const result =props && props.calendar && props.calendar.find(element => element.id === props.num+1)
-    return result ? result.calendarEvent : ''
+    console.log(result, 'res')
+    return result && result.isSubmitted ? result.calendarEvent : ''
   }
 
   const handleTodo = (event) => {
-    props.setTodos({id: props.num+1, open: 'open', calendarEvent: event.target.value});
+    props.setTodos({id: props.num+1, open: 'open', isSubmitted: false, calendarEvent: event.target.value});
   }
 
-  const todoSnippet = <div className={`todo ${calendar.open}`}>  <textarea
-          value={calendar.calendarEvent}
-          onChange={handleTodo}
-          rows={5}
-          cols={5}
-        /> </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const result = props && props.calendar && props.calendar.find(element => element.id === props.num+1)
+    result.isSubmitted = true;
+    console.log(result)
+    // displayCalendar()
+    // props.setTodos({id: props.num+1, open: 'open', calendarEvent: e.target.value});
+    // console.log('hey')
+  }
+
+  const todoSnippet =
+  <form onSubmit={handleSubmit}>
+   <div className={`todo ${calendar.open}`}>  <textarea
+            value={calendar.calendarEvent}
+            onChange={handleTodo}
+            rows={5}
+            cols={5}
+          />
+    </div>
+    <button type="submit">Add</button>
+  </form>
 
   return(
     <div onClick={handleClick} className="card">
